7d6c93ca2b8eb336aec77257b04324325db07b7d
Cas Van Hövell <casvanhovell@macbook-air.home>
2025-02-27 22:41:18 +0100

    Fix SQLite3 issue by switching to better-sqlite3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
+const Database = require("better-sqlite3");
+const db = new Database("planner.db", { verbose: console.log });
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
